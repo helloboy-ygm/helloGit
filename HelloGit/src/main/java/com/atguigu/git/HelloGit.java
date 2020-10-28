66786b3508ed3b600910ca73c8b601a338aba002
helloboy-ygm <123456yanygm>
2020-10-28 20:38:34 +0800

    克隆者添加

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -11,6 +11,7 @@ public class HelloGit {
         System.out.println("主干添加");
         System.out.println("分支添加");
         System.out.println("在线编辑");
+        System.out.println("克隆过来的项目");
         
     }
 }
